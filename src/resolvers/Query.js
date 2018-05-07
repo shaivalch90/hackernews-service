9e76df36b86816de79f39304f5ed82c64d099ce4
shaivalch90 <shaivalch90@gmail.com>
2018-05-07 10:15:25 -0500

    feat(filtering): add filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,10 @@ function person(root, args) {
 };
 function persons() { return people };
 function feed(root, args, context, info) {
-    return context.db.query.links({}, info)
+    const where = args.filter ? {
+        OR: [{url_contains: args.filter}, {description_contains: args.filter}]
+    } : {}
+    return context.db.query.links({where}, info)
 };
 
 module.exports = {
